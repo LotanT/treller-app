9ff204070472615e6944b85fbe8ad1d19fd58a31
Ofek Ben Baruch <Ofekbb@gmail.com>
2022-01-19 18:32:32 +0200

    changes 3

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
 
+import { AppHeader } from './cmps/dynamic-cmp-headers/AppHeader'
+
 import routes from './routes'
 
 
 
 export class RootCmp extends React.Component {
 
+
+
+
     render() {
         return (
             <div>
-                {/* <AppHeader /> */}
+                <AppHeader/>
                 <main>
                 <Switch>
                     {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
