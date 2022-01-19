ea0fa9e42bc4c2ab0ad355e4d6f990b959f5936c
Idan Rofeh <idanrofeh@gmail.com>
2022-01-19 14:51:05 +0200

    mhjv

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,6 @@ const routes = [
         path: '/',
         component: HomePage,
         label: 'Home',
-<<<<<<< HEAD
-        hoha: 'hoha'
-=======
-        hola: 'hola',
-        ok: 'ok'
->>>>>>> ed52db7a3c50ae7c0215fdeae1bf4226542d6f8c
     },
 
 ]
