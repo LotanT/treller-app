38ba1ecf1cc4cdfadd0fc75857a12aba8905365a
Idan Rofeh <idanrofeh@gmail.com>
2022-01-19 14:49:54 +0200

    jyghb

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 // import { HomePage } from './pages/HomePage.jsx'
-import {HomePage} from './pages/HomePage.jsx'
+import { HomePage } from './pages/HomePage.jsx'
 
 const routes = [
     {
         path: '/',
         component: HomePage,
         label: 'Home',
-        hola: 'hola'
+        hoha: 'hoha'
     },
 
 ]
