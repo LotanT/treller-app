d371de60f64e630d8f7b118b2a72f013a886544d
Ofek Ben Baruch <Ofekbb@gmail.com>
2022-02-07 09:53:18 +0200

    final changes and clean ups

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,14 +1,5 @@
 import io from 'socket.io-client'
-
-export const SOCKET_EMIT_USER_WATCH = 'user-watch';
-export const SOCKET_EVENT_USER_UPDATED = 'user-updated';
-export const SOCKET_EVENT_REVIEW_ADDED = 'review-added';
-export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you';
-
-
 export const SOCKET_EVENT_BOARD_ADDED = 'board-added';
-
-
 export const SOCKET_EMIT_LOGIN = 'set-user-socket';
 export const SOCKET_EMIT_LOGOUT = 'unset-user-socket';
 
@@ -43,49 +34,6 @@ function createSocketService() {
   }
   return socketService
 }
-
-// eslint-disable-next-line
-// function createDummySocketService() {
-//   var listenersMap = {}
-//   const socketService = {
-//     listenersMap,
-//     setup() {
-//       listenersMap = {}
-//     },
-//     terminate() {
-//       this.setup()
-//     },
-//     on(eventName, cb) {
-//       listenersMap[eventName] = [...(listenersMap[eventName]) || [], cb]
-//     },
-//     off(eventName, cb) {
-//       if (!listenersMap[eventName]) return
-//       if (!cb) delete listenersMap[eventName]
-//       else listenersMap[eventName] = listenersMap[eventName].filter(l => l !== cb)
-//     },
-//     emit(eventName, data) {
-//       if (!listenersMap[eventName]) return
-//       listenersMap[eventName].forEach(listener => {
-//         listener(data)
-//       })
-//     },
-//     debugMsg() {
-//       this.emit('chat addMsg', {from: 'Someone', txt: 'Aha it worked!'})
-//     },
-//   }
-//   window.listenersMap = listenersMap;
-//   return socketService
-// }
-
-
-// Basic Tests
-// function cb(x) {console.log('Socket Test - Expected Puk, Actual:', x)}
-// socketService.on('baba', cb)
-// socketService.on('baba', cb)
-// socketService.on('baba', cb)
-// socketService.on('mama', cb)
-// socketService.emit('baba', 'Puk')
-// socketService.off('baba', cb)
 
 
 socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
