5b45f516ddec673de5b6ff9431cdbf71698cb38c
Idan Rofeh <idanrofeh@gmail.com>
2022-03-13 18:38:11 +0200

    Merge branch 'main' of https://github.com/Ofekbb/Treller-App

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -5,7 +5,7 @@ export const SOCKET_EMIT_LOGOUT = 'unset-user-socket';
 
 export const socketService = createSocketService()
 
-const baseUrl = (process.env.NODE_ENV === 'production')? '' : '//localhost:3030'
+const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030'
 
 
 socketService.setup()
@@ -20,7 +20,7 @@ function createSocketService() {
     on(eventName, cb) {
       socket.on(eventName, cb)
     },
-    off(eventName, cb=null) {
+    off(eventName, cb = null) {
       if (!socket) return;
       if (!cb) socket.removeAllListeners(eventName)
       else socket.off(eventName, cb)
@@ -36,7 +36,7 @@ function createSocketService() {
 }
 
 
-socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
-  console.log('Review about me!', review);
-})
+// socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
+//   console.log('Review about me!', review);
+// })
 
